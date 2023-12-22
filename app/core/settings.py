12bbc6028bc12fb4b5f1e89906f67495554cce8c
Asrar Tamim <01asratamim@gmail.com>
2023-12-23 02:08:16 +0600

    changed  the dataset  sqlite3 to mysql

--- app/core/settings.py
+++ app/core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_new',
+        'USER': 'root',
+        'PASSWORD': 'Tm610857!',
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',
     }
 }
 
